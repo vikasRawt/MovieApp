5d7bfdb0100b5e9b89fea7fe5b3b1de5c20a945e
vikas <vicky582050@gmail.com>
2023-09-25 14:53:24 +0530

    cast commit

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import "./Cast.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImg/Img";
+import Img from "../../../components/lazyLoadimg/Img";
 import avatar from "../../../assets/avatar.png";
 
 const Cast = ({ data, loading }) => {
